ba7167d94ec77fcca9c79399a0472de74e805071
Zain Jaafar <zainjaafar120@gmail.com>
2024-07-23 12:07:57 +1200

    changed config for rich text editor

--- news_site/news_site/settings.py
+++ news_site/news_site/settings.py
@@ -174,9 +174,9 @@ customColorPalette = [
 # CKEDITOR_5_CUSTOM_CSS = 'path_to.css' # optional
 # CKEDITOR_5_FILE_STORAGE = "path_to_storage.CustomStorage" # optional
 CKEDITOR_5_CONFIGS = {
-    'default': {
+    'paper_description': {
         'toolbar': ['heading', '|', 'bold', 'italic', 'link',
-                    'bulletedList', 'numberedList', 'blockQuote', 'imageUpload', ],
+                    'bulletedList', 'numberedList', 'blockQuote', 'imageUpload', 'underline', 'removeFormat'],
 
     },
     'extends': {
